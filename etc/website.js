018596727b45653b1d754fa38befc84c1f887316
Mikhail Matveev <xtreme@rh1.ru>
2024-06-17 00:30:47 +0300

    Updates

--- etc/website.js
+++ etc/website.js
@@ -1,8 +1,17 @@
-const meta = require("./meta.json");
-
 const config = {
     url: "https://hereticjs.org",
-    ...meta,
+    title: {
+        "en-us": "Heretic Web Framework"
+    },
+    shortTitle: {
+        "en-us": "Heretic"
+    },
+    description: {
+        "en-us": "Heretic is the framework which allows you to build a blazing fast and modern website which combines both SSR and SPA approaches"
+    },
+    webmaster: {
+        "en-us": "Mikhail Matveev"
+    },
 };
 
 module.exports = config;
